dca77901911377dfbd8bcf4ee94c99662c3fa370
bskim <gimbosng@gmail.com>
2023-03-18 00:11:50 +0900

    문제1-1) 고객별 영화 대여 또는 반납 메뉴 기능 구현

--- C#/Customer.cs
+++ C#/Customer.cs
@@ -5,69 +5,77 @@ using System.Text;
 
 namespace VideoRental
 {
-  class Customer
-  {
-    public Customer(string name)
+    class Customer
     {
-        customerName = name;
-    }
-
-    public void addRental(Rental arg) { customerRental.Add(arg); }
-    public string getName() { return customerName; }
-
-    public string statement()
-    {
-        double totalAmount = 0.0;
-        int frequentRenterPoints = 0;
-        StringBuilder result = new StringBuilder();
-
-        result.AppendLine("Rental Record for" + getName());
-
-
-        IEnumerator<Rental> enumerator = customerRental.GetEnumerator();
-
-        for (; enumerator.MoveNext();)
+        public Customer(string name)
         {
-            double thisAmount = 0.0;
-            Rental each = enumerator.Current;
-
-            switch (each.getMovie().getPriceCode())
-            {
-                case Movie.REGULAR:
-                    thisAmount += 2.0;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-
-            // Add frequent renter points
-            frequentRenterPoints++;
-
-            // Add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    && each.getDaysRented() > 1) frequentRenterPoints++;
-
-            // Show figures for this rental
-            result.AppendLine("\t" + each.getMovie().getTitle() + "\t" + thisAmount.ToString());
-            totalAmount += thisAmount;
+            customerName = name;
         }
 
-        result.AppendLine("Amount owed is " + totalAmount);
-        result.AppendLine("You earned " + frequentRenterPoints + " frequent renter points");
+        public void addRental(Rental arg) { customerRental.Add(arg); }
+        public string getName() { return customerName; }
 
-        return result.ToString();
+        public bool removeRental(string title)
+        {
+            if (customerRental.Count == 0) return false;
+            int index = customerRental.FindIndex(x => x.getMovie().getTitle() == title);
+            if (index < 0) return false;
+            customerRental.RemoveAt(index);
+            return true;
+        }
+        public string statement()
+        {
+            double totalAmount = 0.0;
+            int frequentRenterPoints = 0;
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine("Rental Record for " + getName());
+
+
+            IEnumerator<Rental> enumerator = customerRental.GetEnumerator();
+
+            for (; enumerator.MoveNext();)
+            {
+                double thisAmount = 0.0;
+                Rental each = enumerator.Current;
+
+                switch (each.getMovie().getPriceCode())
+                {
+                    case Movie.REGULAR:
+                        thisAmount += 2.0;
+                        if (each.getDaysRented() > 2)
+                            thisAmount += (each.getDaysRented() - 2) * 1.5;
+                        break;
+                    case Movie.NEW_RELEASE:
+                        thisAmount += each.getDaysRented() * 3;
+                        break;
+
+                    case Movie.CHILDRENS:
+                        thisAmount += 1.5;
+                        if (each.getDaysRented() > 3)
+                            thisAmount += (each.getDaysRented() - 3) * 1.5;
+                        break;
+                }
+
+                // Add frequent renter points
+                frequentRenterPoints++;
+
+                // Add bonus for a two day new release rental
+                if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+                        && each.getDaysRented() > 1) frequentRenterPoints++;
+
+                // Show figures for this rental
+                result.AppendLine("\t" + each.getMovie().getTitle() + "\t" + thisAmount.ToString());
+                totalAmount += thisAmount;
+            }
+
+            result.AppendLine("Amount owed is " + totalAmount);
+            result.AppendLine("You earned " + frequentRenterPoints + " frequent renter points");
+
+            return result.ToString();
+        }
+
+        private string customerName;
+        private List<Rental> customerRental = new List<Rental>();
     }
-
-    private string customerName;
-    private List<Rental> customerRental = new List<Rental>();
-  }
 }
--- C#/Program.cs
+++ C#/Program.cs
@@ -2,29 +2,278 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+// Add
+using System.IO;
 
 namespace VideoRental
 {
     class Program
     {
+        enum Menu
+        {
+            Main = 0,
+            PrintAllVideoTitle,
+            Rental,
+            Return,
+            SaveFile,
+            Receipt,
+            Exit
+        }
+
+        static List<Movie> movieList = new List<Movie>();
+        static Dictionary<string, Customer> customerList = new Dictionary<string, Customer>();
+
         static void Main(string[] args)
         {
-            Movie regular1 = new Movie("일반 1", Movie.REGULAR);
-            Movie regular2 = new Movie( "일반 2", Movie.REGULAR);
-            Movie newRelease1 = new Movie( "신작 1", Movie.NEW_RELEASE );
-            Movie newRelease2 = new Movie( "신작 2",Movie.NEW_RELEASE );
-            Movie children1 = new Movie( "어린이 1", Movie.CHILDRENS );
-            Movie children2 = new Movie( "어린이 2", Movie.CHILDRENS );
-            Customer customer = new Customer("고객");
+            Menu currentMenu = Menu.Main;
+            string menu = "", id = "", title = "", periodStr = "", yesNo = "";
+            int period = 0;
+            int retry = 0, retryMax = 3;
+            bool backMain = false;
 
-            customer.addRental(new Rental( regular1, 2 ));
-            customer.addRental(new Rental( regular2, 3 ));
-            customer.addRental(new Rental( newRelease1, 1 ));
-            customer.addRental(new Rental( newRelease2, 2 ));
-            customer.addRental(new Rental( children1, 3 ));
-            customer.addRental(new Rental( children2, 4 ));
+            movieList.Add(new Movie( "일반1", Movie.REGULAR ));
+            movieList.Add(new Movie( "일반2", Movie.REGULAR ));
+            movieList.Add(new Movie( "신작1", Movie.NEW_RELEASE ));
+            movieList.Add(new Movie( "신작2",Movie.NEW_RELEASE ));
+            movieList.Add(new Movie( "어린이1", Movie.CHILDRENS ));
+            movieList.Add(new Movie( "어린이2", Movie.CHILDRENS ));
 
-            Console.Write(customer.statement());
+            do
+            {
+                switch (currentMenu)
+                {
+                    case Menu.Main:
+                        {
+                            Console.WriteLine(printMainView());
+                            Console.Write("Select Menu: ");
+                            menu = Console.ReadLine();
+                            Console.WriteLine("------------------");
+                            currentMenu = (Menu)(Convert.ToInt32(menu) + 1);
+                        }
+                        break;
+
+                    // Rental 샵에서 보유중인 모든 비디오 정보를 출력
+                    case Menu.PrintAllVideoTitle:
+                        {
+                            Console.WriteLine("----Movie List-----");
+                            foreach (Movie m in movieList)
+                            {
+                                Console.WriteLine(printMovieInfo(m));
+                            }
+                            Console.WriteLine("------------------");
+                            currentMenu = Menu.Main;
+                        }
+                        break;
+
+                    // Rental 메뉴로 이동
+                    case Menu.Rental:
+                        {
+                            Movie movie = null;
+                            retry = 0;
+                            backMain = false;
+
+                            Console.WriteLine("-----Rental Menu-----");
+                            Console.Write("Input customer ID :");
+                            id = Console.ReadLine();
+                            if (!customerList.ContainsKey(id))
+                            {
+                                Console.Write("입력한 정보와 일치하는 회원이 없습니다. ID를 새로 만드시겠습니까? (Y/N) : ");
+                                yesNo = Console.ReadLine();
+                                if (yesNo.ToLower().Trim() == "y")
+                                    customerList.Add(id, new Customer(id));
+                                else
+                                {
+                                    Console.WriteLine("------------------");
+                                    currentMenu = Menu.Main;
+                                    break;
+                                }
+                            }
+                            do
+                            {
+                                Console.Write("Input Video Title :");
+                                title = Console.ReadLine();
+                                if (title.ToLower().Trim() == "m")
+                                {
+                                    backMain = true;
+                                    break;
+                                }
+                                movie = movieList.Find(x => x.getTitle() == title);
+                                if (movie != null)
+                                    break;
+                                else
+                                    Console.WriteLine(string.Format("입력한 제목과 일치하는 영화가 없습니다. 다시 입력해주세요. [{0} / {1}] (M 입력 시, Main 화면으로 이동)", retry, retryMax));
+                                retry++;
+                            } while (retry < retryMax);
+                            if (movie != null)
+                            {
+                                bool enterOK = false;
+                                retry = 0;
+                                do
+                                {
+                                    Console.Write("Input Period :");
+                                    periodStr = Console.ReadLine();
+                                    enterOK = int.TryParse(periodStr, out period);
+                                    if (enterOK)
+                                        break;
+                                    else
+                                        Console.WriteLine(string.Format("입력한 숫자가 올바르지 않습니다. 다시 입력해주세요. [{0} / {1}]", retry, retryMax));
+                                    retry++;
+                                } while (retry < retryMax);
+                                if (enterOK)
+                                    rentMovie(id, movie, period);
+                            }
+                            Console.WriteLine("------------------");
+                            if (backMain)
+                            {
+                                currentMenu = Menu.Main;
+                            }
+                            else
+                            {
+                                // Y입력 시 Rental 메뉴 반복 N 입력 시 Main Menu로 이동
+                                Console.Write("Continue? (Y/N) : ");
+                                yesNo = Console.ReadLine();
+                                currentMenu = yesNo.ToLower().Contains("y") ? Menu.Rental : Menu.Main;
+                            }
+                        }
+                        break;
+
+                    // Return 메뉴로 이동
+                    case Menu.Return:
+                        {
+                            retry = 0;
+                            backMain = false;
+
+                            Console.WriteLine("-----Return Menu-----");
+                            Console.Write("Input customer ID :");
+                            id = Console.ReadLine();
+                            if (!customerList.ContainsKey(id))
+                            {
+                                Console.WriteLine("입력한 정보와 일치하는 회원이 없습니다.");
+                            }
+                            else
+                            {
+                                do
+                                {
+                                    Console.Write("Input Video Title :");
+                                    title = Console.ReadLine();
+                                    if (title.ToLower().Trim() == "m")
+                                    {
+                                        backMain = true;
+                                        break;
+                                    }
+
+                                    if (returnMovie(id, title))
+                                        break;
+                                    else
+                                        Console.WriteLine(string.Format("입력한 제목의 대여 기록이 없습니다. 다시 입력해주세요. [{0} / {1}] (M 입력 시, Main 화면으로 이동)", retry, retryMax));
+                                    retry++;
+                                } while (retry < retryMax);
+
+                            }
+                            Console.WriteLine("------------------");
+                            if (backMain)
+                            {
+                                currentMenu = Menu.Main;
+                            }
+                            else
+                            {
+                                // Y입력 시 Return 메뉴 반복 N 입력 시 Main Menu로 이동
+                                Console.Write("Continue? (Y/N) : ");
+                                yesNo = Console.ReadLine();
+                                currentMenu = yesNo.ToLower().Contains("y") ? Menu.Return : Menu.Main;
+                            }
+                        }
+                        break;
+
+                    // 현재 Rental 한 모든 고객 정보를 영수증 스타일로 파일로 저장
+                    case Menu.SaveFile:
+                        Console.WriteLine("-----Save Receipt-----");
+                        foreach (Customer customer in customerList.Values)
+                        {
+                            Console.WriteLine(saveReceipt(customer));
+                        }
+                        Console.WriteLine("------------------");
+                        currentMenu = Menu.Main;
+                        break;
+
+                    // 고객ID를 입력받아 해당 고객이 대여한 비디오 영수증을 출력
+                    case Menu.Receipt:
+                        Console.WriteLine("-----Print Receipt-----");
+                        Console.Write("Input customer ID :");
+                        id = Console.ReadLine();
+                        if (!customerList.ContainsKey(id))
+                        {
+                            Console.WriteLine("입력한 정보와 일치하는 회원이 없습니다.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("------------------");
+                            Console.WriteLine(printReceipt(customerList[id]));
+                            Console.WriteLine("------------------");
+                        }
+                        Console.WriteLine("------------------");
+                        // Y입력 시 Return 메뉴 반복 N 입력 시 Main Menu로 이동
+                        Console.Write("Continue? (Y/N) : ");
+                        yesNo = Console.ReadLine();
+                        currentMenu = yesNo.ToLower().Contains("y") ? Menu.Receipt : Menu.Main;
+                        break;
+                }
+
+            } while (currentMenu != Menu.Exit);
+        }
+
+        static string printMainView()
+        {
+            StringBuilder sRet = new StringBuilder();
+            sRet.AppendLine("-----Main Menu-----");
+            sRet.AppendLine("0 : Print All Video Title");
+            sRet.AppendLine("1 : Rental");
+            sRet.AppendLine("2 : Return");
+            sRet.AppendLine("3 : Save to File");
+            sRet.AppendLine("4 : Receipt");
+            sRet.AppendLine("5 : Exit");
+            return sRet.ToString();
+        }
+
+        static string printMovieInfo(Movie movie)
+        {
+            if (movie.getPriceCode() == 0)
+                return string.Format("[REGULAR] {0}", movie.getTitle());
+            else if (movie.getPriceCode() == 1)
+                return string.Format("[NEW_RELEASE] {0}", movie.getTitle());
+            else if (movie.getPriceCode() == 2)
+                return string.Format("[CHILDRENS] {0}", movie.getTitle());
+            else
+                return "";
+        }
+
+        static void rentMovie(string customerID, Movie movie, int period)
+        {
+            customerList[customerID].addRental(new Rental(movie, period));
+        }
+
+        static bool returnMovie(string customerID, string title)
+        {
+            return customerList[customerID].removeRental(title);
+        }
+
+        static string saveReceipt(Customer customer)
+        {
+            try
+            {
+                string fileName = Path.Combine(Directory.GetCurrentDirectory(), "Receipt_" + customer.getName());
+                File.WriteAllText(fileName, customer.statement());
+                return "Receipt_" + customer.getName() + ".txt";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        static string printReceipt(Customer customer)
+        {
+            return customer.statement();
         }
     }
 }
